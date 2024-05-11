8fe1e7760b1c9e8d2a69acbf3cc76b9c3126b39d
YaKuu=SmorGle <smorgleyt@gmail.com>
2024-05-11 12:12:12 -0300

    commit

--- Interface.py
+++ Interface.py
@@ -16,7 +16,15 @@ from customtkinter import *
 def ver_hora():
     global agora_string
     agora = dt.datetime.now()
-    agora_string = agora.strftime("%I:%M:%S")    
+    hora = agora.strftime("%I")  
+    minuto = agora.strftime("%M")  
+    segundo = agora.strftime("%S")  
+
+    horaconvertida = int(hora)
+    horasomada = horaconvertida + 12
+
+    horareal = hora + ":" + minuto + ":" + segundo
+  
     label_hora = CTkLabel(janela, text=agora_string)
     label_hora.place(x=15, y=10)
     label_hora.after(100,ver_hora)
