f50e406bca5ee0f60fe6780c906cbd0eac437ddc
adamys245 <caraestranha2@gmail.com>
2024-05-14 17:01:31 -0300

    commit

--- Interface.py
+++ Interface.py
@@ -1,24 +1,66 @@
-from tkinter import *
 import datetime as dt
 from customtkinter import *
 
-# import pygame as pg
-#fkfkkf
-#cwncchingi
-#rlr
 
 # def despertar():
 #     alarme = ('09:01:00')#so colocar um input nisso ou chockbox e ja era
 
-#     if agora_string == alarme and ligado == True:
-#         print("despertou!!")
+
         
+def pegar_info():
+    global cont,alarmes
+    # pegar horas
+    pegar_hora = box_hora.get()
+    pegar_min = box_min.get()
+    pegar_seg = box_seg.get()
     
+    # pegar dias
+    pegar_1=seg.get()
+    pegar_2=ter.get()
+    pegar_3=quar.get()
+    pegar_4=quin.get()
+    pegar_5=sex.get()
+    pegar_6=sab.get()
+    pegar_7=dom.get()
+    
+    
+    dias_para_despertar = []#vou fazer um for, vai guardar o dia que é pra despertar
+
+    if pegar_1 != 0: 
+        dias_para_despertar.append(pegar_1)
+    if pegar_2 != 0:
+        dias_para_despertar.append(pegar_2)
+    if pegar_3 != 0:
+        dias_para_despertar.append(pegar_3)
+    if pegar_4 != 0:
+        dias_para_despertar.append(pegar_4)
+    if pegar_5 != 0:
+        dias_para_despertar.append(pegar_5)
+    if pegar_6 != 0:
+        dias_para_despertar.append(pegar_6)
+    if pegar_7 != 0:
+        dias_para_despertar.append(pegar_7)
+    alarme_inteiro=[] #onde fica armazenado o alarme inteiro 
+                # o dia: a hora : e segundos.
+    for dia in dias_para_despertar:
+
+
+         var_alarme_inteiro = f"""{dia} {pegar_hora}:{pegar_min}:{pegar_seg}"""#tentar fazer um variavel que muda automaticamente
+         alarme_inteiro.append(var_alarme_inteiro)
+    
+    cont=1
+    
+    
+    
+    janela_add_clock.destroy()
+    
+
+
 def ver_hora():
-    global agora_string
+    global agora_string,agora,dia
     agora = dt.datetime.now()
     dia_atual = agora.strftime("%w")
-    if dia_atual == '7':
+    if dia_atual == '0':
         dia = "Domingo"      
     if dia_atual == '6':
         dia = "Sábado"  
@@ -32,15 +74,13 @@ def ver_hora():
         dia = "Terça"
     if dia_atual == '1':
         dia = "Segunda"
-    
-    agora_string = agora.strftime(f"""
-{dia}
-%H:%M:%S""") 
+        
+    agora_string = agora.strftime(f"""{dia} %H:%M:%S""")
     
    
 
-    label_hora = CTkLabel(janela, text=agora_string)
-    label_hora.place(x=15, y=10)
+    label_hora = CTkLabel(janela, text=agora_string,font=('Arial',10))
+    label_hora.place(x=3, y=10)
     
     
     label_hora.after(100,ver_hora)
@@ -50,14 +90,17 @@ def ver_hora():
 cont=1
 # funçao abrir nova janela
 def nova_janela():
-    global cont
-    global janela_add_clock
+    global janela_add_clock,cont,box_hora,box_min,seg,box_seg,ter,quar,quin,sex,sab,dom
     if cont==1:
         cont+=1
 
         janela_add_clock = CTk()
         
-        janela_add_clock.geometry('400x300')
+        janela_add_clock.title("")
+        
+        # janela_add_clock.iconphoto(False,"imagens.png","png")
+        
+        janela_add_clock.geometry('420x300')
         
         janela_add_clock.resizable(width=FALSE,height=False)
         # horas
@@ -72,35 +115,65 @@ def nova_janela():
         minutos = tuple(str(i).zfill(2) for i in range(60))
 
         label_min = CTkLabel(janela_add_clock, text="Minutos",font=("Arial",20))
-        label_min.place(x=100, y=20)
+        label_min.place(x=170, y=20)
         
         box_min = CTkComboBox(janela_add_clock,values = minutos,width=60,height=30)
-        box_min.place(x=100, y=50)
+        box_min.place(x=170, y=50)
         
         # segundos
         segundos = tuple(str(i).zfill(2) for i in range(60))
         
         label_min = CTkLabel(janela_add_clock, text="Segundos",font=("Arial",20))
-        label_min.place(x=200, y=20)
+        label_min.place(x=320, y=20)
         
-        box_min = CTkComboBox(janela_add_clock,values = segundos,width=60,height=30)
-        box_min.place(x=210, y=50)
+        box_seg = CTkComboBox(janela_add_clock,values = segundos,width=60,height=30)
+        box_seg.place(x=320, y=50)
         
         # DIAS_________________
-        seg = CTkCheckBox(janela_add_clock,text='Seg',onvalue='')
-        seg.place(x=10, y=10)
+        seg = CTkCheckBox(janela_add_clock,text='Seg',onvalue='Segunda',offvalue=0)
+        seg.place(x=10, y=90)
+        
+        ter = CTkCheckBox(janela_add_clock,text='Ter',onvalue='Terça',offvalue=0)
+        ter.place(x=70, y=90)
+        
+        quar = CTkCheckBox(janela_add_clock,text='Quar',onvalue='Quarta',offvalue=0)
+        quar.place(x=125, y=90)
+        
+        quin = CTkCheckBox(janela_add_clock,text='Quin',onvalue='Quinta',offvalue=0)
+        quin.place(x=185, y=90)
+        
+        sex = CTkCheckBox(janela_add_clock,text='Sext',onvalue='Sexta',offvalue=0)
+        sex.place(x=245, y=90)
+        
+        sab = CTkCheckBox(janela_add_clock,text='Sab',onvalue='Sábado',offvalue=0)
+        sab.place(x=305, y=90)
+        
+        dom = CTkCheckBox(janela_add_clock,text='Dom',onvalue='Domingo',offvalue=0)
+        dom.place(x=360, y=90)
+        # Button_pegar_info
+        button_pegar_info = CTkButton(janela_add_clock,text="Adicionar alarme",command=pegar_info)
+        button_pegar_info.place(x=140, y=180)
+        
 
-        janela_add_clock.mainloop() 
+        janela_add_clock.mainloop()
+        
+
 
     else:
-        janela_add_clock.destroy()
         cont=1
+        janela_add_clock.destroy()
+        
+        
+        
+
+    
 
 janela = CTk()
 
 janela.config(bg= "#363636")
 
 janela.geometry("800x500")
+
 
 janela.resizable(width=FALSE,height=False)
 
@@ -117,11 +190,6 @@ label_crono.place(x=300, y=10)
 label_lado=CTkLabel(janela, text='',height=700,width=80)
 label_lado.grid(row=0,column=0)
 
-
-
-
-
-    
     
 
 # Coloca as funções aqui apolo
@@ -133,8 +201,9 @@ botao_remover_alarme.place(x=28, y=440)
 
 
 
-
 ver_hora()
 
 
 janela.mainloop()
+
+
--- teste.py
+++ teste.py
@@ -1,18 +1,22 @@
-import datetime 
+import datetime  as dt
 
-while True:
-    # Usaremos essa estrutura para sempre atualizar o valor da hora/minutos/segundos
-    agora = datetime.datetime.now()
-    # agora_string = agora.strftime("%I:%M:%S")
-    print(agora)
-    break
+agora = dt.datetime.now()
+dia_atual = agora.strftime("%w")
+print(dia_atual)
+
+# while True:
+#     # Usaremos essa estrutura para sempre atualizar o valor da hora/minutos/segundos
+#     agora = datetime.datetime.now()
+#     # agora_string = agora.strftime("%I:%M:%S")
+#     print(agora)
+#     break
     
     
     
-print(datetime.time(1,3,5))
+# print(datetime.time(1,3,5))
 
-#podemos usar ele para quando o horario chegar ele despertar
-# é possível criarmos um data e um tempo, ou criarmos uma variável que tenha tanto a data quanto a hora, usando ele
+# #podemos usar ele para quando o horario chegar ele despertar
+# # é possível criarmos um data e um tempo, ou criarmos uma variável que tenha tanto a data quanto a hora, usando ele
 
-# if hora_limite == hora_atual:
-#     desperter
+# # if hora_limite == hora_atual:
+# #     desperter
